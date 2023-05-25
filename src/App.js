ed143ef88bd904f1e4b0d6e02b33a2cc602da68f
Maria Rios Navarro <propscode254@gmail.com>
2023-05-25 14:47:28 +0200

    Add Activity Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import Form from "./components/Form/Form.js";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
-  return <Form />;
+  const [activityState, setActivityState] = useState("");
+
+  function handleAddActivity(newActivity) {
+    setActivityState([{ id: uid(), ...newActivity }, ...activityState]);
+  }
+
+  // onAddActivity is a Prop of Form and we give the function as value.
+
+  return <Form onAddActivity={handleAddActivity} />;
 }
 
 export default App;
